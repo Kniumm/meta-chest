000c23a0661c3e00b04ba42cf3c34a9bc4c82581
Eliott Wattelier <eliott.wattelier@nyll.tech>
2026-08-29 18:35:30 +0200

    Remove unused mixins

--- src/client/java/kniumm/metachest/client/ChestRenderStateExtension.java
+++
@@ -1,9 +0,0 @@
-package kniumm.metachest.client;
-
-import net.minecraft.client.resources.model.sprite.SpriteId;
-
-public interface ChestRenderStateExtension {
-    void metachest$setCustomSprite(SpriteId sprite);
-
-    SpriteId metachest$getCustomSprite();
-}
